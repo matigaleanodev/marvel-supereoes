f8332ce3c0c138ff3b1569dae8525874e2417f23
Matias Galeano <99203645+matigaleanodev@users.noreply.github.com>
2025-02-22 10:23:01 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'marver-superheroes',
+  webDir: 'www'
+};
+
+export default config;
