e770a18ca15f06e4d89e1ccb40ecd17f6abbd92e
Matias Galeano <99203645+matigaleanodev@users.noreply.github.com>
2025-02-24 16:55:55 -0300

    test(i18n): implementacion pruebas unitarias para servicio y pipe de traducción

--- src/app/shared/pipes/translate/translate.pipe.spec.ts
+++ src/app/shared/pipes/translate/translate.pipe.spec.ts
@@ -1,8 +1,46 @@
 import { TranslatePipe } from './translate.pipe';
 
 describe('TranslatePipe', () => {
-  it('create an instance', () => {
+  it('debería crear una instancia del pipe', () => {
     const pipe = new TranslatePipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('debería devolver la traducción correcta para una key válida', () => {
+    const pipe = new TranslatePipe();
+
+    const translateServiceMock = {
+      pipeTranslate: (key: string, lang: string) => {
+        if (key === 'xCargandoDatos' && lang === 'es') {
+          return 'Cargando Datos';
+        }
+        return key;
+      },
+      currentLang: () => 'es',
+    };
+
+    pipe['_service'] = translateServiceMock as any;
+
+    const result = pipe.transform('xCargandoDatos');
+    expect(result).toBe('Cargando Datos');
+  });
+
+  it('debería devolver debería devolver la key si la traducción no se encuentra', () => {
+    const pipe = new TranslatePipe();
+
+    const translateServiceMock = {
+      pipeTranslate: (key: string, lang: string) => {
+        if (key === 'xCargandoDatos' && lang === 'es') {
+          return 'Cargando Datos';
+        }
+        return key;
+      },
+      currentLang: () => 'es',
+    };
+
+    pipe['_service'] = translateServiceMock as any;
+
+    const result = pipe.transform('xNoKey');
+    expect(result).toBe('xNoKey');
   });
 });
--- src/app/shared/services/translate/translate.service.spec.ts
+++ src/app/shared/services/translate/translate.service.spec.ts
@@ -1,6 +1,8 @@
 import { TestBed } from '@angular/core/testing';
 
 import { TranslateService } from './translate.service';
+import { Language } from '@shared/enums/language.enum';
+import { esAR } from '@shared/i18n/esAR';
 
 describe('TranslateService', () => {
   let service: TranslateService;
@@ -10,7 +12,55 @@ describe('TranslateService', () => {
     service = TestBed.inject(TranslateService);
   });
 
-  it('should be created', () => {
+  it('deberia crear una instancia del servicio', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('debería inicializar el idioma y setear currentLang al idioma del dispositivo', () => {
+    const spy = spyOnProperty(navigator, 'language', 'get').and.returnValue(
+      'es-AR'
+    );
+
+    service.initLang();
+
+    expect(service.getCurrentLanguage()).toBe(Language.ES);
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('debería devolver el idioma actual', () => {
+    expect(service.getCurrentLanguage()).toBe(Language.ES);
+
+    service['setLanguage'](Language.EN);
+    expect(service.getCurrentLanguage()).toBe(Language.EN);
+  });
+
+  it('constante de traduccion debería devolver el valor traducido para una key válida', () => {
+    const key = 'xCargandoDatos';
+    const translation = esAR[key];
+
+    service['setLanguage'](Language.ES);
+    expect(service.instant(key)).toBe(translation);
+  });
+
+  it('constante de traduccion debería devolver la key si la traducción no se encuentra', () => {
+    const key = 'xNoKey';
+
+    expect(service.instant(key)).toBe(key);
+
+    service['setLanguage'](Language.EN);
+    expect(service.instant(key)).toBe(key);
+  });
+
+  it('metodo instant debería devolver la traducción para una key válida', () => {
+    const key = 'xCargandoDatos';
+    const translation = service.instant(key);
+    expect(translation).toBe('Cargando datos');
+  });
+
+  it('metodo instant debería devolver la key si la traducción no se encuentra', () => {
+    const key = 'xNoKey';
+    const translation = service.instant(key);
+    expect(translation).toBe(key);
   });
 });
